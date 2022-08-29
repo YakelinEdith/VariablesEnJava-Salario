270303a96b2b09a03cc28e68ae1388cd05c970f5
YakelinEdith <yakelinedith10@gmail.com>
2022-08-29 15:11:56 -0600

    Borrando variables string

--- src/main/java/com/mycompany/calculodesalario/CalculoDeSalario.java
+++ src/main/java/com/mycompany/calculodesalario/CalculoDeSalario.java
@@ -21,18 +21,7 @@ public class CalculoDeSalario {
       salaryFinal=salary;
       
       System.out.println(salaryFinal);
-        // Actualizar variables de tipo String:
-      String employeeName = "Anahí ";
-      employeeName = employeeName + "Salgado ";
-      System.out.println(employeeName); // Anahí Salgado, "ojo" el error era que la variable que 
-                                        // se iba a imprimir era el salario y no la variable en la 
-                                        // cual tenemos almacenado el nombre
-      
-      employeeName += "Díaz de la Vega";
-      System.out.println(employeeName); // Anahí Salgado Díaz de la Vega
-      
-      employeeName = "Irene " + employeeName;
-      System.out.println(employeeName); //Irene Anahí Salgado Díaz de la Vega
+
     }
 }    
       
